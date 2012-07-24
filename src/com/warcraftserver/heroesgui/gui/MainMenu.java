0530fefdc596a97b77ce51054859c6d424a46007
Aaron Somers <somers.ans@gmail.com>
2012-07-24 16:45:53 -0400

    minor changes

--- src/com/warcraftserver/heroesgui/gui/MainMenu.java
+++ src/com/warcraftserver/heroesgui/gui/MainMenu.java
@@ -33,28 +33,24 @@ public class MainMenu {
         uuidMainMenu.put(sp, screenId);
         mainMenuInstance.put(sp, this);
         
-        GenericTexture label1 = new GenericTexture();
-        label1.setUrl(sp.getSkin());
-        label1.setHeight(46).setWidth(230);
-        label1.setPriority(RenderPriority.High);
-        label1.setAnchor(WidgetAnchor.TOP_CENTER);
-        label1.shiftXPos(-115);
+        int height = sp.getMainScreen().getHeight();
+        int width = sp.getMainScreen().getWidth();
         
         GenericEntityWidget entityWidget = new GenericEntityWidget(sp.getEntityId());
         entityWidget.setDirty(true);
         entityWidget.setHeight(46).setWidth(230);
         entityWidget.setPriority(RenderPriority.High);
         entityWidget.setAnchor(WidgetAnchor.CENTER_CENTER);
-//        entityWidget.shiftXPos(-115);
+        entityWidget.setX((int)(.1 * width));
+        entityWidget.setY((int)(.1 * height));
         
         GenericButton button1 = new GenericButton("Close Menu");
-        button1.setAlign(WidgetAnchor.CENTER_CENTER);
+        button1.setX((int)(.1 * width));
+        button1.setY((int)(.1 * height));
         button1.setAnchor(WidgetAnchor.CENTER_CENTER);
-//        button1.shiftXPos(40).shiftYPos(80);
         button1.setHeight(15).setWidth(200);
         button1.setHoverColor(new Color(1.0F, 5, 0, 1.0F));
         
-//        pop.attachWidget(HeroesGUI.getInstance(), label1);
         pop.attachWidget(HeroesGUI.getInstance(), entityWidget);
         pop.attachWidget(HeroesGUI.getInstance(), button1);
         pop.setDirty(true);
